14260e5c064d5623c1af9db7110f9bab28597c12
hidepon3110 <hidepon3110@gmail.com>
2018-11-27 00:14:09 +0900

    feat: app4

--- main.js
+++ main.js
@@ -29,3 +29,17 @@ var app3 = new Vue({
     out:true
   }
 })
+
+/**
+ * 
+ */
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
